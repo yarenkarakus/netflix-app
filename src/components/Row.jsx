e550933375c1647053b685913a483c635233b462
Yaren Karakus <yarenkarakus007@gmail.com>
2023-02-05 14:13:38 +0300

    row component created

---
+++ src/components/Row.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Row = ({ title, fetchURL }) => {
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+    </>
+  );
+};
+
+export default Row;
