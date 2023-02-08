096e4e6ad2ba0c2bbbf2f3e2d27590d230b1a06b
Yaren Karakus <yarenkarakus007@gmail.com>
2023-02-08 12:10:41 +0300

    deleted console.log

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,6 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
-  // console.log(movie);
 
   const truncateString = (str, num) => {
     if (str?.length > num) {
