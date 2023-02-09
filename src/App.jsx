9460bcf717e264036a893a47ac419727c45aa35a
Yaren Karakus <yarenkarakus007@gmail.com>
2023-02-09 23:43:38 +0300

    feat: alert message

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Play from "./pages/Play";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route path="/play" element={<Play />} />
         </Routes>
       </AuthContextProvider>
     </>
